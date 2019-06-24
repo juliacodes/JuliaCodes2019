a95f38325dc4533011779e1cc5317201ab00bbd0
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-06-24 19:34:40 +0200

    Prettier Styling changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Landing from './Landing'
-
+import React from "react";
+import Landing from "./Landing";
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      <Landing />
     </div>
   );
 }
--- src/Landing.js
+++ src/Landing.js
@@ -1,49 +1,50 @@
-import React, { Component } from 'react';
-import styled from 'styled-components'
-import Hamburger from './components/Hamburger'
-
+import React, { Component } from "react";
+import styled from "styled-components";
+import Hamburger from "./components/Hamburger";
 
 const HomeContainer = styled.div`
-    width: 100vw;
-    height: 100vh;
-    
-    padding: 60px;
-`
+  width: 100vw;
+  height: 100vh;
+
+  padding: 60px;
+`;
 
 const Blue = styled.div`
-    background-color: #ECF3F4;
-    height: 100%;
+  background-color: #ecf3f4;
+  height: 100%;
 
-    & h1{
-        position: relative;
-        font-size: 200px;
-        line-height: 91.45%;
-        padding: 300px 390px;
-    }
-`
+  & h1 {
+    position: relative;
+    font-size: 200px;
+    line-height: 91.45%;
+    padding: 250px 0 0 300px;
+  }
+`;
 const Cutout = styled.div`
-    position: absolute;
-    width: 850px;
-    height: 0;
-    border-top: 90vh solid white;
-    border-right: 700px solid transparent;
-`
-
+  position: absolute;
+  width: 600px;
+  height: 0;
+  border-top: 1100px solid white;
+  border-right: 700px solid transparent;
+`;
 
 class Landing extends Component {
-    render() {
-        return (
-            <div>
-                <HomeContainer>
-                    <Blue>
-                        <Hamburger></Hamburger>
-                        <Cutout />
-                        <h1>JU <br />LIA</h1>
-                    </Blue>
-                </HomeContainer>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <HomeContainer>
+          <Blue>
+            <Hamburger></Hamburger>
+            <Cutout />
+            <h1>
+              JU <br />
+              LIA
+            </h1>
+          </Blue>
+        </HomeContainer>
+      </div>
+    );
+  }
 }
 
 export default Landing;
