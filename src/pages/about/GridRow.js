246463e3de39c9ea066212ca470068f8d507bf7d
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-07-02 22:42:57 +0200

    Separated Components

---
+++ src/pages/about/GridRow.js
@@ -0,0 +1,101 @@
+import React from "react";
+import styled from "styled-components/macro";
+import comp from "./comp.png";
+import coffee from "./coffee.png";
+import ipad from "./ipad.png";
+import sticker from "./sticker.png";
+
+const Row = styled.div`
+  width: 100%;
+  margin-top: 50px;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const LeftGrid = styled.div`
+  flex: 70%;
+  flex-wrap: wrap;
+  padding: 0px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas: "Left Left centertop Right Right" "Left Left centerbot Right Right";
+  grid-gap: 20px;
+
+  * {
+    position: relative;
+    height: 200px;
+  }
+`;
+
+const GridLeft = styled.div`
+  grid-area: Left;
+  height: 420px;
+  background-image: url(${comp});
+  background-size: cover;
+`;
+const CenterTop = styled.div`
+  grid-area: centertop;
+  background-image: url(${sticker});
+  background-size: cover;
+`;
+const CenterBot = styled.div`
+  grid-area: centerbot;
+  background-image: url(${coffee});
+  background-size: cover;
+`;
+const GridRight = styled.div`
+  grid-area: Right;
+  height: 420px;
+  background-image: url(${ipad});
+  background-size: cover;
+`;
+
+const RightCTA = styled.div`
+  flex: 30%;
+  padding: 20px;
+  align-items: center;
+  display: flex;
+`;
+
+const CTAText = styled.div`
+  flex: 1 0 auto;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20%;
+  background-position: center center;
+
+  h3 {
+    max-width: 250px;
+    margin: 30px auto;
+    text-align: left;
+    font-family: "Lato", sans-serif;
+    font-size: 28px;
+    line-height: 128.95%;
+    font-weight: 800;
+    letter-spacing: -0.025em;
+    margin-bottom: 30px;
+  }
+`;
+
+function GridRow() {
+  return (
+    <Row>
+      <LeftGrid>
+        <GridLeft></GridLeft>
+        <CenterTop></CenterTop>
+        <CenterBot></CenterBot>
+        <GridRight></GridRight>
+      </LeftGrid>
+      <RightCTA>
+        <CTAText>
+          <h3>I'm a lot cooler on instagram</h3>
+        </CTAText>
+      </RightCTA>
+    </Row>
+  );
+}
+
+export default GridRow;
---
+++ src/pages/about/IntroRow.js
@@ -0,0 +1,102 @@
+import React from "react";
+import styled from "styled-components/macro";
+import Me from "./Me.png";
+
+const Row = styled.div`
+  width: 100%;
+  margin-top: 50px;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Left = styled.div`
+  flex: 55%;
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const HeaderText = styled.div`
+  padding: 0;
+  width: 50%;
+  min-width: 200px;
+  flex: 1 0 auto;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20%;
+  background-position: center center;
+
+  h1 {
+    font-family: "Lato", sans-serif;
+    font-size: 42px;
+    line-height: 128.95%;
+    font-weight: 800;
+    letter-spacing: -0.025em;
+    margin-bottom: 30px;
+  }
+
+  p {
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 156.19%;
+    margin-bottom: 30px;
+    color: #5e5e5e;
+    letter-spacing: 0.03em;
+  }
+`;
+
+const Right = styled.div`
+  flex: 45%;
+  height: auto;
+  display: flex;
+  position: relative;
+`;
+
+const RightImage = styled.div`
+  width: 100%;
+  height: 50vh;
+  max-height: 800px;
+  min-height: 500px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center right;
+  background-image: url(${Me});
+`;
+
+function IntroRow() {
+  return (
+    <Row>
+      <Left>
+        <HeaderText>
+          <h1>
+            Building More Than <br />
+            Just Websites
+          </h1>
+          <p>
+            Duis sit amet mauris in tortor facilisis ornare eleifend vel quam.
+            Aliquam erat volutpat. Integer non elementum sem, vel convallis
+            erat.
+          </p>
+          <p>
+            Duis sit amet mauris in tortor facilisis ornare eleifend vel quam.
+            Aliquam erat volutpat. Integer non elementum sem, vel convallis
+            erat.
+          </p>
+          <p>
+            Duis sit amet mauris in tortor facilisis ornare eleifend vel quam.
+            Aliquam erat volutpat. Integer non elementum sem, vel convallis
+            erat.
+          </p>
+        </HeaderText>
+      </Left>
+      <Right>
+        <RightImage />
+      </Right>
+    </Row>
+  );
+}
+
+export default IntroRow;
