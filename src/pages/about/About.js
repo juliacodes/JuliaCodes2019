f5cd0477bbe570e0705c88ca6fcc31e41f65994b
juliajohnson18 <32076859+juliajohnson18@users.noreply.github.com>
2019-07-02 22:17:50 +0200

    Added images to grid row

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import styled from "styled-components/macro";
 import Hamburger from "../../components/hamburger";
 import Me from "./Me.png";
+import comp from "./comp.png";
+import coffee from "./coffee.png";
+import ipad from "./ipad.png";
+import sticker from "./sticker.png";
 
 const Cursor = styled.div``;
 const Mouse = styled.div``;
@@ -193,6 +197,73 @@ const Timeline = styled.div`
   }
 `;
 
+const LeftGrid = styled.div`
+  flex: 70%;
+  flex-wrap: wrap;
+  padding: 0px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas: "Left Left centertop Right Right" "Left Left centerbot Right Right";
+  grid-gap: 20px;
+
+  * {
+    position: relative;
+    height: 200px;
+  }
+`;
+
+const GridLeft = styled.div`
+  grid-area: Left;
+  height: 420px;
+  background-image: url(${comp});
+  background-size: cover;
+`;
+const CenterTop = styled.div`
+  grid-area: centertop;
+  background-image: url(${sticker});
+  background-size: cover;
+`;
+const CenterBot = styled.div`
+  grid-area: centerbot;
+  background-image: url(${coffee});
+  background-size: cover;
+`;
+const GridRight = styled.div`
+  grid-area: Right;
+  height: 420px;
+  background-image: url(${ipad});
+  background-size: cover;
+`;
+
+const RightCTA = styled.div`
+  flex: 30%;
+  padding: 20px;
+  align-items: center;
+  display: flex;
+`;
+
+const CTAText = styled.div`
+  flex: 1 0 auto;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20%;
+  background-position: center center;
+
+  h3 {
+    max-width: 250px;
+    margin: 30px auto;
+    text-align: left;
+    font-family: "Lato", sans-serif;
+    font-size: 28px;
+    line-height: 128.95%;
+    font-weight: 800;
+    letter-spacing: -0.025em;
+    margin-bottom: 30px;
+  }
+`;
+
 class About extends Component {
   state = {
     xMain: 0,
@@ -320,6 +391,19 @@ class About extends Component {
             </RightTriad>
           </Row>
           <RowSpacer />
+          <Row>
+            <LeftGrid>
+              <GridLeft></GridLeft>
+              <CenterTop></CenterTop>
+              <CenterBot></CenterBot>
+              <GridRight></GridRight>
+            </LeftGrid>
+            <RightCTA>
+              <CTAText>
+                <h3>I'm a lot cooler on instagram</h3>
+              </CTAText>
+            </RightCTA>
+          </Row>
           <Hamburger />
         </Content>
       </Layout>
